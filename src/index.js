2f406febc69655fdab6f3e160147e5fcdc7f5624
alopsantos <anderson_lops@hotmail.com>
2020-01-18 15:49:04 -0300

    Adicionado cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -10,6 +11,7 @@ mongoose.connect('mongodb+srv://omnistack:Mongo91196355@omnistack-hsvvq.mongodb.
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
